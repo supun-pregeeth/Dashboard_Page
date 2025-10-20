5d0060a309fcaa4932e4463a6d090bab772ca376
supun-pregeeth <ruchiraniasanka@gmail.com>
2025-10-20 16:07:13 +0530

    Creating admin page

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,10 +1,25 @@
-import React from "react";
+import { useNavigate } from 'react-router-dom';
 import "./Login.css";
+import { useState } from 'react';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate(); 
 
   const handleLogin = (e) => {
+
     e.preventDefault();
+
+    if(email ==="admin@gmail.com" && password ==="123456"){
+      localStorage.setItem("admin", true);
+      navigate("/admin");
+
+    }else{
+      alert("Invalid credentials");
+    }
+
+    console.log("Login attempted with", email, password);
     
   }
 
@@ -18,14 +33,18 @@ function Login() {
         type="email" 
         placeholder="Email" 
         className="login-input"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
       />
       <input 
         type="password" 
         placeholder="Password" 
         className="login-input"
+        value={password}
+        onChange={(e)=>setPassword(e.target.value)}
       />
 
-      <button className="login-button">Login</button>
+      <button className="login-button" onClick={handleLogin}>Login</button>
 
       <div className="login-options">
         <a href="/forgot-password" className="login-link">Forgot Password?</a>
