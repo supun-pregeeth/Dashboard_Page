222515873dad548235033df07508eff484e011f1
supun-pregeeth <ruchiraniasanka@gmail.com>
2025-11-02 00:08:30 +0530

    user table

--- src/Pages/Admin.js
+++ src/Pages/Admin.js
@@ -1,7 +1,59 @@
-import React from 'react'
+import "./Admin.css";
 
 export default function Dashboard() {
+  const overviewData =[
+    {title: "Total Users", value: "78"},
+    {title: "Active Users", value: "45"},
+    {title: "New Signups", value: "5"},
+    {title: "Pending Requests", value: "1"},
+  ];
+
+  const usersData =[
+    { name: "Supun lakmal", email: "spl@gmail.com"},
+    { name: "Nimal silva", email: "Nimal45@gmail.com"},
+  
+  ];
+
   return (
-    <div>Admin</div>
+    <div className="dashboard-container">
+      <h1 className="dashboard-title">Admin Dashboard</h1>
+      
+      {/* Overview boxes */}
+      
+<div className="all">
+      {overviewData.map((item, index) => (
+     <div key={index} className="overviewDatacard">
+    <h3>{item.title}</h3>
+    <p>{item.value}</p>
+  </div>
+))}
+
+{/* Users table */}
+  <div className="users-table-container">
+    <table className="users-table">
+      <div className="table-header">
+      <thead>
+        <tr >
+          <th>Name</th>
+          <th>Email</th>
+        </tr>
+      </thead>
+      </div>
+      <tbody>
+        {usersData.map(
+          (user,index)=>(
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+      </tbody>
+      
+    </table>
+    
+  </div>
+</div>
+</div>
   )
 }
